cfd833023a6886c58652b95efa17459531870dbf
ahmadjihanatqia <atqiyaahmad@gmail.com>
2017-11-24 13:47:14 +0700

    AboutActivity

--- app/src/main/java/com/tugas/rpl/personalfinancialassistance/DashboardActivity.java
+++ app/src/main/java/com/tugas/rpl/personalfinancialassistance/DashboardActivity.java
@@ -47,8 +47,7 @@ public class DashboardActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AboutActivity.startThisActivity(DashboardActivity.this);
             }
         });
 
