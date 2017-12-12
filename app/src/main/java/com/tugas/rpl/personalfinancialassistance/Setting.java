501866f15013ccdde734de55079892d386b68289
ahmadjihanatqia <atqiyaahmad@gmail.com>
2017-12-12 20:15:28 +0700

    Setting
    Setting jing

---
+++ app/src/main/java/com/tugas/rpl/personalfinancialassistance/Setting.java
@@ -0,0 +1,15 @@
+package com.tugas.rpl.personalfinancialassistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        getSupportActionBar().setTitle("Setting");
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+}
